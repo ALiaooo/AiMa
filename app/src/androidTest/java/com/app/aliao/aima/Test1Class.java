389d2a34ee3f8dcca9b7a31e3f768194edcb8b52
阿廖 <liaolishuang@gmail.com>
2015-03-05 16:19:20 +0800

    commit volley source code

---
+++ app/src/androidTest/java/com/app/aliao/aima/Test1Class.java
@@ -0,0 +1,15 @@
+package com.app.aliao.aima;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by sf on 2014/12/29.
+ * desc:
+ */
+public class Test1Class extends InstrumentationTestCase{
+
+    public void  test() throws Exception{
+        assertEquals(3, 2);
+
+    }
+}
